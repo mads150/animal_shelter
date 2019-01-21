dc99fac4e80229646cd542ad9a1209c21a34cc36
mads150 <botvinnikova90@mail.ru>
2019-01-21 21:02:36 +1000

    minified files

--- js/mini-jennifer-card.js
+++ js/mini-jennifer-card.js
@@ -1,1 +1,1 @@
-
+var button=document.querySelector(".jennifer-button");var popup=document.querySelector(".modal-jennifer");var close=popup.querySelector(".modal-close");button.addEventListener("click",function(evt){evt.preventDefault();popup.classList.add("modal-show")});close.addEventListener("click",function(evt){evt.preventDefault();popup.classList.remove("modal-show")})
--- js/pet-card.js
+++ js/pet-card.js
@@ -1,1 +1,28 @@
-some text
+var button = document.querySelector(".jennifer-button");
+
+var charlyButton = document.querySelector(".charly-button");
+
+var popup = document.querySelector(".modal-jennifer");
+var charlyPopup = document.querySelector(".modal-charly");
+var close = popup.querySelector(".modal-close");
+var charlyClose = charlyPopup.querySelector(".modal-close");
+
+button.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+charlyButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  charlyPopup.classList.add("modal-show");
+});
+
+charlyClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  charlyPopup.classList.remove("modal-show");
+});
